51f091635a252591fe4686f6bf0b8103516fc433
eirture <eirture@gmail.com>
2017-01-03 09:15:48 +0800

    update

--- app/src/main/java/com/eirture/easy/JApplication.java
+++ app/src/main/java/com/eirture/easy/JApplication.java
@@ -2,11 +2,6 @@ package com.eirture.easy;
 
 import android.app.Application;
 
-import com.eirture.easy.main.model.Journal;
-import com.simperium.Simperium;
-import com.simperium.client.Bucket;
-import com.simperium.client.BucketNameInvalid;
-
 import org.androidannotations.annotations.EApplication;
 
 /**
@@ -14,35 +9,8 @@ import org.androidannotations.annotations.EApplication;
  */
 @EApplication
 public class JApplication extends Application {
-
-    private Simperium mSimperium;
-    private Bucket<Journal> mJournalBucket;
-
     @Override
     public void onCreate() {
         super.onCreate();
-        initSimperium();
     }
-
-    private void initSimperium() {
-        mSimperium = Simperium.newClient(
-                BuildConfig.SIMPERIUM_APP,
-                BuildConfig.SIMPERIUM_KEY,
-                this
-        );
-        try {
-            mJournalBucket = mSimperium.bucket(new Journal.Schema());
-        } catch (BucketNameInvalid e) {
-            throw new RuntimeException("Could not create bucket", e);
-        }
-    }
-
-    public Simperium getSimperium() {
-        return mSimperium;
-    }
-
-    public Bucket<Journal> journalBucket() {
-        return mJournalBucket;
-    }
-
 }
