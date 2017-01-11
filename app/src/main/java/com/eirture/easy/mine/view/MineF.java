f841d7a976c23bd37872bcde88851fc24dda254c
eirture <eirture@gmail.com>
2017-01-11 15:23:39 +0800

    update

--- app/src/main/java/com/eirture/easy/mine/view/MineF.java
+++ app/src/main/java/com/eirture/easy/mine/view/MineF.java
@@ -1,17 +1,50 @@
 package com.eirture.easy.mine.view;
 
+import com.eirture.easy.R;
 import com.eirture.easy.main.view.MainFragment;
 
+import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EFragment;
 
 /**
  * Created by eirture on 17-1-10.
  */
-@EFragment
+@EFragment(R.layout.f_mine)
 public class MineF extends MainFragment {
 
     @Override
     protected void refreshNotebook() {
+
+    }
+
+
+    @Click(R.id.item_account)
+    protected void clickAccount() {
+
+    }
+
+    @Click(R.id.item_sync)
+    protected void clickSync() {
+
+    }
+
+    @Click(R.id.item_notebook)
+    protected void clickNotebook() {
+
+    }
+
+    @Click(R.id.item_lock)
+    protected void clickLock() {
+
+    }
+
+    @Click(R.id.item_alert)
+    protected void clickAlert() {
+
+    }
+
+    @Click(R.id.item_about)
+    protected void clickAbout() {
 
     }
 }
