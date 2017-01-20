7249b59252b1e7ba8075c3a322b6f7512d864e16
eirture <eirture@gmail.com>
2017-01-20 18:37:00 +0800

    update lock activity

--- app/src/main/java/com/eirture/easy/base/views/BaseActivity.java
+++ app/src/main/java/com/eirture/easy/base/views/BaseActivity.java
@@ -2,10 +2,13 @@ package com.eirture.easy.base.views;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
+
+import com.eirture.easy.main.view.CheckPasswordA_;
+import com.eirture.easy.mine.data.PasswordPrefs_;
 
 /**
  * Created by eirture on 16-12-4.
@@ -13,29 +16,34 @@ import android.view.View;
 
 public class BaseActivity extends AppCompatActivity {
     private static final String TAG = "BaseActivity";
+    private static final int REQUEST_LOCK_ACTIVITY = 99;
     private static boolean needPassword;
+    private PasswordPrefs_ passwordPrefs;
+
+    private Handler handler = new Handler();
+    private Runnable startLockTask = () ->
+            CheckPasswordA_.intent(this)
+                    .startForResult(REQUEST_LOCK_ACTIVITY);
 
     public void back(View view) {
         this.finish();
     }
 
-
     @Override
     protected void onStart() {
         super.onStart();
-//        if (needPassword) {
-//            CheckPasswordA_.intent(this)
-//                    .startForResult(REQUEST_PASSWORD);
-//        }
-
-        Log.d(TAG, "onStart: start password check: " + needPassword);
-        Log.d(TAG, "onStart: " + getClass().getSimpleName());
+        handler.removeCallbacks(startLockTask);
+        if (needPassword && passwordPrefs.openPassword().get()) {
+            handler.postDelayed(startLockTask, 300);
+        }
+//        Log.d(TAG, "onStart: " + getClass().getSimpleName());
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        handler.removeCallbacks(startLockTask);
         needPassword = false;
-        Log.d(TAG, "onActivityResult: " + getClass().getSimpleName());
+//        Log.d(TAG, "onActivityResult: " + getClass().getSimpleName());
         super.onActivityResult(requestCode, resultCode, data);
     }
 
@@ -43,30 +51,21 @@ public class BaseActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         needPassword = true;
-        Log.d(TAG, "onStop: " + getClass().getSimpleName());
+//        Log.d(TAG, "onStop: " + getClass().getSimpleName());
     }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        passwordPrefs = new PasswordPrefs_(this);
         needPassword = false;
-
-        Log.d(TAG, "onCreate: context package: " + getIntent().getComponent().getPackageName());
-
-        Log.d(TAG, "onCreate: " + getClass().getSimpleName());
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        needPassword = false;
-        Log.d(TAG, "onDestroy: " + getClass().getSimpleName());
+//        Log.d(TAG, "onCreate: " + getClass().getSimpleName());
     }
 
     @Override
     public void finish() {
-        super.finish();
         needPassword = false;
-        Log.d(TAG, "finish: " + getClass().getSimpleName());
+        super.finish();
+//        Log.d(TAG, "finish: " + getClass().getSimpleName());
     }
 }
